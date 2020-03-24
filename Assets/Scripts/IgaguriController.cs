95f56687367cd971a0b61e4fec5ae47f90e5634f
tomoya-takubo <tkb1003@gmail.com>
2020-03-25 02:34:51 +0900

    202003250234　コミット
    イガグリ的に刺さる　まで

---
+++ Assets/Scripts/IgaguriController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IgaguriController : MonoBehaviour
+{
+    public void Shoot(Vector3 dir)
+    {
+        GetComponent<Rigidbody>().AddForce(dir);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        GetComponent<Rigidbody>().isKinematic = true;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Shoot(new Vector3(0, 200, 2000));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
